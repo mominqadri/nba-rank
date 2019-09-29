3cfc5a9f9f8ba4082d5e2660c9cbb2ee46c1e3ff
Brave Hager <bravehager7@gmail.com>
2019-09-29 05:27:12 -0400

    Last push

--- nba-rank-client/src/App.js
+++ nba-rank-client/src/App.js
@@ -1,40 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import { Container, Row } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Player } from "./pages/Player";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { players: [], isLoading: false };
-  }
-
-  fetchPlayers() {
-    this.setState({ isLoading: true }, () => {
-      fetch("https://nba-rank.herokuapp.com/players")
-        .then(res => res.json())
-        .then(res => {
-          console.log("hello");
-          console.log(res);
-          this.setState({ players: res.players });
-        });
-    });
-  }
-
-  componentDidMount() {
-    this.fetchPlayers();
-  }
-
   render() {
-    const { players } = this.state;
     return (
-      <ol>
-        {players.map(({ id, first_name, last_name }) => {
-          return (
-            <ul key={id}>
-              <h1>{{ first_name }}</h1>
-            </ul>
-          );
-        })}
-      </ol>
+      <Router>
+        <Container>
+          <Row>
+            <h1 className="header">
+              <img
+                className="logo"
+                alt="logo"
+                src="https://styles.redditmedia.com/t5_2qo4s/styles/communityIcon_1podsfdai4301.png"
+              ></img>
+              <Link to="/">nba-rank</Link>
+            </h1>
+          </Row>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/players/:id" component={Player}></Route>
+        </Container>
+      </Router>
     );
   }
 }
---
+++ nba-rank-client/src/pages/Player.js
@@ -0,0 +1,135 @@
+import React from "react";
+import { debounce } from "lodash";
+import { Col, Row } from "react-bootstrap";
+import { Player as PlayerComponent } from "../components/Player";
+
+export class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: {},
+      error: false,
+      isLoading: true
+    };
+  }
+
+  fetchPlayer() {
+    this.setState({ isLoading: true }, () => {
+      const { id } = this.props.match.params;
+      fetch(`https://nba-rank.herokuapp.com/players/${id}`)
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            player: res,
+            isLoading: false
+          });
+        });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchPlayer();
+  }
+
+  render() {
+    let {
+      id,
+      first_name,
+      last_name,
+      headshot,
+      num_mentions,
+      sentiment_score,
+      polarity_score,
+      mentions
+    } = this.state.player;
+    console.log(mentions);
+    if (this.state.isLoading) {
+      return <h1>Loading</h1>;
+    }
+    return (
+      <div>
+        <PlayerComponent
+          id={id}
+          firstName={first_name}
+          lastName={last_name}
+          headshot={headshot}
+          numMentions={num_mentions}
+          sentimentScore={sentiment_score}
+          polarityScore={polarity_score}
+        />
+        <Col md={12}>
+          <hr />
+        </Col>
+        <h1>Top Mentions</h1>
+        <Row>
+          <Col md={12}>
+            {mentions
+              .sort(
+                (a, b) =>
+                  Math.abs(b.sentiment) * (1.01 ^ Math.abs(b.upvotes)) -
+                  Math.abs(a.sentiment) * (1.01 ^ Math.abs(a.upvotes))
+              )
+              .slice(0, 5)
+              .map(({ sentence, sentiment, upvotes }, index) => {
+                return (
+                  <div className="mention" key={index}>
+                    <span className="mention-scores">
+                      +
+                      <span
+                        style={{
+                          color: upvotes < 0 ? "red" : "green",
+                          marginRight: "10px"
+                        }}
+                      >
+                        {" "}
+                        {upvotes}
+                      </span>{" "}
+                      {sentiment}
+                    </span>
+                    <br></br>
+                    <span className="mention-quote">
+                      "...{sentence.substr(0, 200)}..."
+                    </span>
+                  </div>
+                );
+              })}
+          </Col>
+        </Row>
+        <Col md={12}>
+          <hr></hr>
+        </Col>
+        <h1>Most Negative Mentions</h1>
+        <Row>
+          <Col md={12}>
+            {mentions
+              .sort((a, b) => a.sentiment - b.sentiment)
+              .slice(0, 5)
+              .map(({ sentence, sentiment, upvotes }, index) => {
+                return (
+                  <div className="mention" key={index}>
+                    <span className="mention-scores">
+                      +
+                      <span
+                        style={{
+                          color: upvotes < 0 ? "red" : "green",
+                          marginRight: "10px"
+                        }}
+                      >
+                        {" "}
+                        {upvotes}
+                      </span>{" "}
+                      {sentiment}
+                    </span>
+                    <br></br>
+                    <span className="mention-quote">
+                      "...{sentence.substr(0, 200)}..."
+                    </span>
+                  </div>
+                );
+              })}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
